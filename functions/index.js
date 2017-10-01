0af556f5c1ef2219bbfdbd0b1b2b119366a8600c
Wescley Matos <wescleymatos@gmail.com>
2017-10-01 10:02:58 -0300

    criação de função para armazenar usuário criado

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.app = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+admin.initializeApp(functions.config().firebase);
+
+const ref = admin.database().ref();
+
+exports.createUserAccount = functions.auth.user().onCreate(event => {
+  let uid = event.data.uid;
+  let email = event.data.email;
+  let photoUrl = event.data.photoUrl || 'https://lh5.googleusercontent.com/-28uFtmZZ0bM/AAAAAAAAAAI/AAAAAAAAAAA/fXkKfFZd11E/s128-c-k/photo.jpg';
+
+  const newUserRef = ref.child(`/users/${uid}`);
+  return newUserRef.set({
+    email: email,
+    photoUrl: photoUrl
+  });
+});
+
+exports.api = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase! Alterado!");
 });
