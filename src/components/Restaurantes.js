793cc63030b8ee4f1dc4db857974c933fa7740af
Wescley Matos <wescleymatos@gmail.com>
2017-10-02 05:40:08 -0300

    implementando busca de restaurantes com visualização no mapa

--- src/components/Restaurantes.js
+++ src/components/Restaurantes.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
 class Restaurantes extends Component {
+  componentDidMount() {
+    alert('teste');
+  }
+
   render() {
     return (
       <div className="row">
         <div className="col-lg-12">
-          <p>Restaurantes</p>
+          <div class="form-group">
+            <input type="search" className="form-control" id="buscar-restaurantes" placeholder="Buscar Restaurantes" />
+          </div>
+          <div id="map"></div>
+          <button type="button" className="btn btn-success">Adicionar novo restaurante</button>
         </div>
       </div>
     );
