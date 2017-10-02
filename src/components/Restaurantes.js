586f99f52d0fe3d3e13a177e15acf447a10ac54d
Wescley Matos <wescleymatos@gmail.com>
2017-10-02 16:50:59 -0300

    setendo se o usuário aceitou a geolocalização

--- src/components/Restaurantes.js
+++ src/components/Restaurantes.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 
 class Restaurantes extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isGettingPosition: false
+    };
+  }
+
   componentDidMount() {
-    alert('teste');
+    this.setState({ isGettingPosition: true });
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({ isGettingPosition: false });
+      console.log(position);
+    });
   }
 
   render() {
     return (
       <div className="row">
         <div className="col-lg-12">
-          <div class="form-group">
+          <div className="form-group">
             <input type="search" className="form-control" id="buscar-restaurantes" placeholder="Buscar Restaurantes" />
           </div>
           <div id="map"></div>
