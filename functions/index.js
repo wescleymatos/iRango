06f97362b5a9d791cc9632960e18193c19fd0c0f
Wescley Matos <wescleymatos@gmail.com>
2017-10-02 23:07:59 -0300

    Api busca restaurantes (#13)
    * implementando inserção de um novo restaurante pela api
    
    * #10 - implementação de busca de restaurantes por nome

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,14 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 admin.initializeApp(functions.config().firebase);
 
 const ref = admin.database().ref();
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 exports.createUserAccount = functions.auth.user().onCreate(event => {
   let uid = event.data.uid;
@@ -17,6 +22,34 @@ exports.createUserAccount = functions.auth.user().onCreate(event => {
   });
 });
 
-exports.api = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase! Alterado!');
+app.get('/restaurants/:name', (req, res) => {
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+
+  let name = req.params.name;
+
+  ref.child('restaurants').orderByChild('name').equalTo(name).once('value')
+    .then((snapshot) => {
+      let result = [];
+      snapshot.forEach((snap) => {
+        result.push(snap.val());
+      });
+
+      res.send({ result });
+    });
 });
+
+app.post('/restaurants', (req, res) => {
+  const data = {
+    name: req.body.name,
+    lat: req.body.lat,
+    lng: req.body.lng
+  };
+
+  ref.child('restaurants').push().set(data)
+    .then(() => ref.child('restaurants').once('value'))
+    .then((snapshot) => {
+      res.send({ result: snapshot.val() });
+    });
+});
+
+exports.api = functions.https.onRequest(app);
