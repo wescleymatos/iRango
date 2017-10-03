97ea44814f63b1e57612f8ff165217306abb1a7e
Wescley Matos <wescleymatos@gmail.com>
2017-10-03 00:22:17 -0300

    setando posição padrão

--- src/components/Restaurantes.js
+++ src/components/Restaurantes.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+
+const Mapa = withScriptjs(withGoogleMap(props => {
+  return (
+    <GoogleMap defaultZoom={16} defaultCenter={{lat: 0, lng: 0}} center={props.center}>
+    </GoogleMap>
+  );
+}));
 
 class Restaurantes extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      isGettingPosition: false
+      isGettingPosition: false,
+      position: {}
     };
   }
 
   componentDidMount() {
     this.setState({ isGettingPosition: true });
     navigator.geolocation.getCurrentPosition(position => {
-      this.setState({ isGettingPosition: false });
-      console.log(position);
+      this.setState({
+        isGettingPosition: false,
+        position: {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+      });
     });
   }
 
@@ -24,7 +39,15 @@ class Restaurantes extends Component {
           <div className="form-group">
             <input type="search" className="form-control" id="buscar-restaurantes" placeholder="Buscar Restaurantes" />
           </div>
-          <div id="map"></div>
+          <div>
+            <Mapa
+              googleMapURL='https://maps.googleapis.com/maps/api/js?key=AIzaSyCQR19677qUdeHnCS36MSzMDLKQzq-d_WA'
+              loadingElement={<div style={{height: '400px', width: '100%'}} />}
+              containerElement={<div style={{height: '400px', width: '100%'}} />}
+              mapElement={<div style={{height: '400px', width: '100%'}} />}
+              center={ this.state.position } />
+          </div>
+          <br />
           <button type="button" className="btn btn-success">Adicionar novo restaurante</button>
         </div>
       </div>
