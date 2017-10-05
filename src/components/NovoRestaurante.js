0077d3c9cd1badb473c496ac305594a65b33ebbc
Wescley Matos <wescleymatos@gmail.com>
2017-10-05 00:39:32 -0300

    add axios para requisições http

--- src/components/NovoRestaurante.js
+++ src/components/NovoRestaurante.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import startup from '../config/startup';
 
 class NovoRestaurante extends Component {
+  submitRestaurante(event) {
+    event.preventDefault();
+
+    const nome = event.target['nome'].value;
+    axios.post(startup.getUrl('restaurants'), {
+      name: nome,
+      lat: 23,
+      lng: -30
+    })
+    .then(function (response) {
+      console.log(response);
+    });
+  }
+
   render() {
     return (
       <div className="row">
         <div className="col-lg-12">
-          <h1>Novo Restaurante</h1>
-          <div className="form-group">
-            <input type="text" className="form-control" placeholder="Nome do restaurante" />
-          </div>
+          <form onSubmit={this.submitRestaurante}>
+            <div className="form-group">
+              <input type="text" name="nome" className="form-control" placeholder="Nome do restaurante" />
+            </div>
+            <button type="submit" className="btn btn-primary">Salvar</button>
+          </form>
         </div>
       </div>
     );
