6746088533f41a45b09713a8076fb904d0af7415
Wescley Matos <wescleymatos@gmail.com>
2017-10-11 13:01:18 -0300

    obtendo dados do firebase

--- functions/controllers/restaurante-controller.js
+++ functions/controllers/restaurante-controller.js
@@ -1,23 +1,23 @@
 const db = require('../config/db');
 const repository = require('../repositories/restaurantes-repository');
 
-// const firebase = require('../repositories/restaurantes-realtime');
-// const functions = require('firebase-functions');
-// const admin = require('firebase-admin');
-// admin.initializeApp(functions.config().firebase, 'DB');
-// const ref = admin.database().ref();
+const firebase = require('../repositories/restaurantes-realtime');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase, 'DB');
+const ref = admin.database().ref();
 
 exports.get = ('/restaurants', (req, res) => {
   res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
 
   //Com firebase
-  // firebase.getAll(ref)
-  //   .then(result => res.send({ result }));
+  firebase.getAll(ref)
+    .then(result => res.send({ result }));
 
   // Com MongoDB
-  repository.getAll(db.connect)
-    .then(result => res.send(result))
-    .catch(err => res.status(400).send(err));
+  // repository.getAll(db.connect)
+  //   .then(result => res.send(result))
+  //   .catch(err => res.status(400).send(err));
 });
 
 exports.post = ('/restaurants', (req, res) => {
@@ -28,13 +28,13 @@ exports.post = ('/restaurants', (req, res) => {
   };
 
   //Com firebase
-  // firebase.create(ref, data)
-  //   .then(() => res.send({msg: 'Restaurante inserido com sucesso.'}));
+  firebase.create(ref, data)
+    .then(() => res.send({msg: 'Restaurante inserido com sucesso.'}));
 
   // Com MongoDB
-  repository.create(db.connect, data)
-    .then(() => res.status(200).send({msg: 'Restaurante inserido com sucesso.'}))
-    .catch(err => res.status(400).send(err));
+  // repository.create(db.connect, data)
+  //   .then(() => res.status(200).send({msg: 'Restaurante inserido com sucesso.'}))
+  //   .catch(err => res.status(400).send(err));
 });
 
 exports.delete = ('/restaurants/:id', (req, res) => {
